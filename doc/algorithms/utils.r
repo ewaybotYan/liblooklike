db50925994e5ebcbcc99a406bd0bfd26854fb108
Nicolas Granger <nicolas.granger@telecom-sudparis.eu>
2013-11-09 15:36:52 +0100

    wip

--- doc/algorithms/utils.r
+++ doc/algorithms/utils.r
@@ -35,16 +35,19 @@ random_deflation_matrix <- function(z, h){
 	D <- diag( runif(n,0,1) )
 
 	for(i in 1:floor(runif(1,n/2,n)) ){
-		j<-floor(runif(1,1,n))
-		k<-floor(runif(1,1,n))
-		D[k,k]<-( D[j,j] + (h * sqrt(z[k]^2+z[j]^2) / ( abs(z[k]*z[j]) ) ) ) #almost the same eigenvalues
-		m<-floor(runif(1,1,n))
+	#	j<-floor(runif(1,1,n))
+	#	k<-floor(runif(1,1,n))
+	#	D[k,k]<-( D[j,j] + (h * sqrt(z[k]^2+z[j]^2) / ( abs(z[k]*z[j]) ) ) ) #almost the same eigenvalues
+	#	m<-floor(runif(1,1,n))
 		r<-floor(runif(1,1,n))
-		D[m,m]<-D[r,r] #same eigenvalues
+		D[r,r]<-D[i,i] #same eigenvalues
 	}
 
-	#random base 
+	#random base
 	P <- matrix(runif(n*n,-10,10),n,n)
-	P <- P + t(P)
-	return(solve(P) %*% D %*% P)
+	A <- solve(P) %*% D %*% P
+	A <- A %*% t(A)
+	D <- D %*% D # todo : optimise in real project
+	
+	return( list( A=A, D=D) )
 }
