4ba43af4102ba558fbcefe2762dd67074e7286b9
Gabriel <gabriel.de_longeaux@tem-tsp.eu>
2013-11-20 21:31:23 +0100

    Complete householder

--- doc/algorithms/householder.r
+++ doc/algorithms/householder.r
@@ -6,8 +6,23 @@ householder_symetry_vector <- function( u, k ){
   return (v)
 }
 
-P <- function( A, k ){
+P_k <- function( A, k ){
   n <- dim(A)[1]
   v = householder_symetry_vector( A[,k], k )
   return( diag(n) - 2 * ( v %*% t(v) ) / ( norm(v)^2 ) )
+}
+
+householder_matrix <- function( A ){
+	n<-sqrt(length(A))
+
+	H<-P(A,1)
+	A<-H%*%A%*%t(H)
+
+	for (i in 2:n){
+		P<-P_k(A,i)
+		H<-P%*%H
+		A<-P%*%A%*%t(P)
+	}
+	A
+	return(H)
 }
