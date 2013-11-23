4d0e35416f7fd201c7b3889e083a3bf27e662bee
Gabriel <gabriel.de_longeaux@tem-tsp.eu>
2013-11-23 14:52:09 +0100

    Eigenvectors of the secular equation

--- doc/algorithms/secular_equation.r
+++ doc/algorithms/secular_equation.r
@@ -47,14 +47,79 @@ psi <- function(v,d,i,x){
 	return( list(Psi1=Psi1,Psi2=Psi2) )
 }
 
-roots_secular_equation <- function (p,v,D,rate){ #d trié ou non?
-	d...
+roots_secular_equation <- function (p,v,D,rate){ #d trié ou non? ATT: rate compris entre 0 et 1 + ATT: valeurs aux bornes à gérer avec p
+
 	n <- length(v)
 	sum <- trace(D)
-	sum_k <- 0
+	d... #à voir
 	j <- 0
-	while ((sum_k/sum)<rate){
-	DPsi <- dPsi(v,d,n-j-1,(d[n-j-1]+d[n-j])/2)
-	find_approx_lambda(c1,c2,c3,d[n-j-1],d[n-j])
+	i<-0 #number of eigenvalues found
+
+	for (ind in 1:n){
+		lambda_k[ind]=0
 	}
+
+	#et si p=0?
+	if (p>0){
+		lambda <- #?
+		sum_lambdak <- lambda
+		i<-i+1
+		lambda_k[n+1-i]<-lambda
+	}
+	else{
+		sum_lambdak <- 0
+	}
+
+
+	while ( ((sum_k/sum)<rate) '&& (j>n-1)' ){
+
+		lambda <- (d[n-j-1]+d[n-j])/2
+
+		for (k in 1:3){
+		Psi <- psi(v,d,n-j-1,lambda)
+		DPsi <- dPsi(v,d,n-j-1,lambda)
+		c1 <- DPsi[1]*((d[n-j-1]-lambda)^2)
+		c2 <- DPsi[1]*((d[n-j]-lambda)^2)
+		c3 <- 1+Psi[1]+Psi[2]-DPsi[1]*(d[n-j-1]-lambda)-DPsi[2]*(d[n-j]-lambda)
+		lambda=find_approx_lambda(c1,c2,c3,d[n-j-1],d[n-j])
+		}
+
+	sum_lambdak <- sum_lambdak+lambda
+	j <- j+1
+	i<-i+1
+	lambda_k[n+1-i]<-lambda
+	}
+
+
+	if (p<0){
+		lambda <- #?
+		sum_lambdak <- sum_lambdak+lambda
+		i<-i+1
+		lambda_k[n+1-i]<-lambda
+	}
+
+return (lambda_k)
+}
+
+eigen_vector(p,n,d,lambda) <- function{ #d trié
+
+	for (k in 1:n){
+	p1=p2=p3=p4=1
+
+		for (j in 1:(k-1)){
+			p1 <- p1*(d[k]-lambda[j])
+		}
+		for (j in k:n){
+			p2 <- p2*(lambda[j]-d[k])
+		}
+		for (j in 1:(k-1)){
+			p3 <- p3*(d[k]-d[j])
+		}
+		for (j in (k+1):n){
+			p4 <- p4*(d[j]-d[k])
+		}
+
+	v[k]<-sqrt((p1*p2)/(p*p3*p4))
+	}
+
 }
