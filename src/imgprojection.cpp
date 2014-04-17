f9fa9cc82d9f09ce93648398c6210c92f42c2bfd
Nicolas Granger <nicolas.granger@telecom-sudparis.eu>
2014-04-17 21:30:33 +0100

    preparing projection program

---
+++ src/imgprojection.cpp
@@ -0,0 +1,12 @@
+/**
+ * @author Nicolas Granger <nicolas.granger@telecom-sudparis.eu>
+ * @file   imgprojection.cpp
+ * @date   16/04/2014
+ */
+
+
+int main( int argc, char* argv[] )
+{
+
+  return 0;
+}
