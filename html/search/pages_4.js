695122fc7a8f3bd619750537e467bcb8383e4769
Nicolas Granger <nicolas.granger@telecom-sudparis.eu>
2014-05-02 21:42:43 +0100

    updated gh-pages with improved documentation

--- html/search/pages_4.js
+++ html/search/pages_4.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['todo_20list',['Todo List',['../todo.html',1,'']]],
-  ['tree_20evaluation',['Tree evaluation',['../tree_evaluation.html',1,'']]]
+  ['tree_20evaluation',['Tree evaluation',['../tree_evaluation.html',1,'']]],
+  ['tutorials',['Tutorials',['../tutorials.html',1,'']]]
 ];
